6a80b3b3d775c18108976dfdde14956579df436e
unknown <raghavendiran46461@gmail.com>
2024-04-12 15:24:13 +0530

    Day4 - Doctor Class : Q1

---
+++ Day4/Application/Doctor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    internal class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Experience { get; set; }
+        public string Qualification { get; set; }
+        public string Speciality { get; set; }
+        public Doctor(int id)
+        {
+            Id = id;    
+        }
+        /// <summary>
+        /// Parameterized Constructor
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <param name="exp"></param>
+        /// <param name="qual"></param>
+        /// <param name="spec"></param>
+        public Doctor(int id, string name, int age, int exp, string qual, string spec)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Experience = exp;   
+            Qualification = qual;   
+            Speciality = spec;  
+
+        }
+        /// <summary>
+        /// Prints all Details of the Doctor
+        /// </summary>
+        public void PrintDoctorDetails()
+        {
+            Console.WriteLine($"Doctor Id\t:\t{Id}");
+            Console.WriteLine($"Doctor Name\t:\t{Name}");
+
+            Console.WriteLine($"Doctor Age\t:\t{Age}");
+
+            Console.WriteLine($"Doctor Experience\t:\t{Experience}");
+
+            Console.WriteLine($"Doctor Qualification\t:\t{Qualification}");
+
+            Console.WriteLine($"Doctor Speciality\t:\t{Speciality}");
+
+        }
+
+    }
+}
---
+++ Day4/Application/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Application
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Doctor doctor = new Doctor(1, "Ram", 23,2,"MBBS","Hematologists");
+            doctor.PrintDoctorDetails();
+        }
+    }
+}
