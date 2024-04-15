f7ae54ea8b8ce57689cc0c4c000836b4e775685b
unknown <raghavendiran46461@gmail.com>
2024-04-15 15:30:31 +0530

    Day5 - RequestTracker : CRUD Operation

---
+++ Day5/RequestTrackerSolution/RequestTrackerModelLibrary/Employee.cs
@@ -0,0 +1,61 @@
+﻿namespace RequestTrackerModelLibrary
+{
+    public class Employee
+    {
+        int age;
+        DateTime dob;
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+        }
+        public DateTime DateOfBirth
+        {
+            get => dob;
+            set
+            {
+                dob = value;
+                age = ((DateTime.Today - dob).Days) / 365;
+            }
+        }
+        public double Salary { get; set; }
+
+        public Employee()
+        {
+            Id = 0;
+            Name = string.Empty;
+            Salary = 0.0;
+            DateOfBirth = new DateTime();
+        }
+        public Employee(int id, string name, DateTime dateOfBirth, double salary)
+        {
+            Id = id;
+            Name = name;
+            DateOfBirth = dateOfBirth;
+            Salary = salary;
+        }
+
+        public void BuildEmployeeFromConsole()
+        {
+            Console.WriteLine("Please enter the Name");
+            Name = Console.ReadLine() ?? String.Empty;
+            Console.WriteLine("Please enter the Date of birth");
+            DateOfBirth = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Please enter the Basic Salary");
+            Salary = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine("Employee Id : " + Id);
+            Console.WriteLine("Employee Name " + Name);
+            Console.WriteLine("Date of birth : " + DateOfBirth);
+            Console.WriteLine("Employee Age : " + Age);
+            Console.WriteLine("Employee Salary : Rs." + Salary);
+        }
+    }
+}
