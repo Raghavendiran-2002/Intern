cbd7c4b6623809e0d0aec66ef478e391ca8253d0
unknown <raghavendiran46461@gmail.com>
2024-04-16 14:30:02 +0530

    Day6 - RequestTracker : OOPS Practice session

---
+++ Day6/RequestTrackerSolution-OOPS/RequestTrackerModelLibrary/Department.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerModelLibrary
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Department_Head { get; set; }
+    }
+}
---
+++ Day6/RequestTrackerSolution-OOPS/RequestTrackerModelLibrary/PermanentEmployee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RequestTrackerModelLibrary
+{
+    public class PermanentEmployee : Employee
+    {
+        public PermanentEmployee()
+        {
+            Type = "PermanentEmployee";
+        }
+        public PermanentEmployee(int id, string name, DateTime dateOfBirth, double salary)
+        {
+            Salary = salary;
+
+        }
+        public override void BuildEmployeeFromConsole()
+        {
+            base.BuildEmployeeFromConsole();
+            Console.WriteLine("Please enter the Basic Salary");
+            Salary = Convert.ToDouble(Console.ReadLine());
+        }
+        public override void PrintEmployeeDetails()
+        {
+            base.PrintEmployeeDetails();
+            Console.WriteLine("Employee Salary : Rs." + Salary);
+        }
+        public void SpecialPermanentMethod()
+        {
+            Console.WriteLine("Special permanent method");
+        }
+    }
+}
