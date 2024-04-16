27c87bd044e9fc786ad75513075cb32e92ae5c3d
unknown <raghavendiran46461@gmail.com>
2024-04-16 16:49:54 +0530

    Day6 - EmployeeBenfits : Interface & Inheritance

---
+++ Day6/EmployeeBenefits/EmployeeBenefitsSolution/EmployeeBenefits/GovtRulesInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBenefits
+{
+    public interface GovtRulesInterface
+    {
+        double EmployeePF(double basicSalary);
+        string LeaveDetails();
+        double GratuityAmount(float serviceCompleted ,double basicSalary);
+    }
+}
---
+++ Day6/EmployeeBenefits/EmployeeBenefitsSolution/EmployeeBenefits/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeeBenefits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CTS ctsEmployee = new CTS(101, "Tharun" , "HR", "Manager" , 50000);
+            Accenture accentureEmployee = new Accenture(102, "Pradesh", "Software", "Lead", 100000);
+            Console.WriteLine("\nCTS Employee : \n");
+            Console.WriteLine(ctsEmployee+ "\n");
+            Console.WriteLine($"Employee PF : {ctsEmployee.EmployeePF(ctsEmployee.BasicSalary)}\n{ctsEmployee.LeaveDetails()}\nGratuity Amount : {ctsEmployee.GratuityAmount(6,ctsEmployee.BasicSalary)}");
+          
+           
+            Console.WriteLine("\n\nAccenture Employee : \n");
+            Console.WriteLine(accentureEmployee);
+            Console.WriteLine($"Employee PF : {accentureEmployee.EmployeePF(accentureEmployee.BasicSalary)}\n{accentureEmployee.LeaveDetails()}\nGratuity Amount : {accentureEmployee.GratuityAmount(6, accentureEmployee.BasicSalary)}");
+        }
+    }
+}
