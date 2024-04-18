acfad91bef0a1cd43e79f5a0b22fe89d30bac24c
unknown <raghavendiran46461@gmail.com>
2024-04-18 17:08:41 +0530

    Day8 - OOPS : polymorphism

---
+++ Day8/OPPS/Day 8 OOPS/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8_OOPS
+{
+    public class Shape
+    {
+        // A few example members
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+
+        // Virtual method
+        public virtual void Draw()
+        {
+            Console.WriteLine("Performing base class drawing tasks");
+        }
+    }
+}
