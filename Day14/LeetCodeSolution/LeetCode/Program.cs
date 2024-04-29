9d44a339d746875464ba21f2fc4718d4a4cac9d8
unknown <raghavendiran46461@gmail.com>
2024-04-29 18:36:46 +0530

    Day14 : Leetcode - MinimumDepthOfBinaryTree

---
+++ Day14/LeetCodeSolution/LeetCode/Program.cs
@@ -0,0 +1,17 @@
+﻿using MinimumDepthOfBinaryTree;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        public void MinimumDepthOfBinaryTree(){
+            BinaryTree bt = new BinaryTree();
+            Console.WriteLine( "Minimum Depth Of Binary Tree : "+bt.MinDepth(bt.tree));
+        }
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.MinimumDepthOfBinaryTree();
+        }
+    }
+}
