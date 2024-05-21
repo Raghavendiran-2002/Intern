6f86f4506e50e096fd46f8b4848e5dadaeaae031
unknown <raghavendiran46461@gmail.com>
2024-05-21 12:44:18 +0530

    Day29 - Project Initialized : Pharmacy Management System

---
+++ MiniProject [ Day29 - Day34 ]/PharmacyManagementSystemSolution/PharmacyManagementSystem/Program.cs
@@ -0,0 +1,43 @@
+namespace PharmacyManagementSystem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddLogging(l => l.AddLog4Net());
+            builder.Services.AddSwaggerGen();
+
+            #region contexts
+            #endregion
+
+            #region repositories
+            #endregion
+
+            #region services
+            #endregion
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
